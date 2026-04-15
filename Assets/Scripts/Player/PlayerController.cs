73131ed3d566cb6b9bd73b87009fcfbf108af1ff
Haykal <ferramentas@haykal.com.br>
2026-04-15 13:50:05 -0300

    Player Anim

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Reference")] 
+    [SerializeField] private Animator _anim;
+
+    [Header("Jump Parameters")]
     [SerializeField] private float _jumpForce = 10.0f;
     [SerializeField] private float _gravityModifier = 1.0f;
     [SerializeField] private bool _isGrounded;
 
-    // private bool _isGameOver = false;
+    private bool _isGameOver;
     private Rigidbody _rb;
 
     public static event Action OnGameOver;
@@ -15,16 +19,24 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
+        
         Physics.gravity *= _gravityModifier; //  gravity = gravity * _gravityModifier
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded) //Verifica se o player apertou espaço e se o personagem está no chão
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded && !_isGameOver) //Verifica se o player apertou espaço e se o personagem está no chão
         {
-            _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
-            _isGrounded = false; // Por estar no ar, a boolean torna-se falsa, impedindo que o if statement seja lido
+            OnJump();
         }
+    }
+
+    private void OnJump()
+    {
+        _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        _isGrounded = false; // Por estar no ar, a boolean torna-se falsa, impedindo que o if statement seja lido
+        _anim.SetTrigger("Jump_trig");
     }
 
     private void OnCollisionEnter(Collision collision) //Se o player estiver colidindo com algo
@@ -37,7 +49,9 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over!");
-            // _isGameOver = true;
+            _isGameOver = true;
+            _anim.SetBool("Death_b", true);
+            _anim.SetInteger("DeathType_int", 1);
             OnGameOver?.Invoke();
         }
     }
